d9302ac651bd5f912c6b24b5c9e9c51cca439c8f
Ariana Guilllermina Latorre <ariana.latorre@globant.com>
2021-05-13 13:24:05 -0300

    Spring initializr

---
+++ src/test/java/course/springframeworkguru/messagesapirestg/MessagesApiRestGApplicationTests.java
@@ -0,0 +1,13 @@
+package course.springframeworkguru.messagesapirestg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagesApiRestGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
