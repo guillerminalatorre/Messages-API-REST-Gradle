6d2b740b5b8d75c6a53c3326b757043db083f7b9
Ariana Guilllermina Latorre <ariana.latorre@globant.com>
2021-05-13 15:47:36 -0300

    fixing

---
+++ src/main/java/course/springframeworkguru/MessagesApiRestGApplication.java
@@ -0,0 +1,13 @@
+package course.springframeworkguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesApiRestGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesApiRestGApplication.class, args);
+	}
+
+}
